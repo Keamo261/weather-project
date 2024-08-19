597c471f40c77cdf5f444513378cf810c7a31bbf
Keamo261 <keamogetswe261@gmail.com>
2024-08-19 15:22:11 +0200

    Updated the icon to inject from javascript

--- src/javascript.js
+++ src/javascript.js
@@ -7,6 +7,10 @@ function refreshWeather(response) {
   let speedElement = document.querySelector("#speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let icon = document.querySelector("#icon-element");
+
+  icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="icon"
+    />`;
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
@@ -39,7 +43,7 @@ function formatDate(date) {
 
 function searchCity(city) {
   let apiKey = "21a849d40ft0a45f399f9odf6fa4fb86";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
@@ -53,4 +57,4 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Pretoria");
+searchCity("Athens");
